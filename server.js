0a82dcf8a052a5b91da9c5823afdc30ff02cb6a6
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-13 11:09:03 +0530

    server

--- server.js
+++ server.js
@@ -33,11 +33,9 @@ connectionLimit: 100,
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', 'https://hathibrand.in');
   next();
 });
-
 
 
 
