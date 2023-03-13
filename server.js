353a1b8f986a227477b18dfcaecb34fa86ba522d
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-13 11:25:25 +0530

    server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 dotenv.config({path:"./config.env"})
 
 app.use(cors({
-  origin: 'https://hathibrand.in/',
+  origin: ['https://hathibrand.in/','http://localhost:3000'],
   credentials: true,
 }));
 
