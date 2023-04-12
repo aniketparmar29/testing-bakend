45d042a5b2df5a72aefe72076647cb8e1eb34565
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-12 17:19:17 +0530

    ASDF

--- server.js
+++ server.js
@@ -84,6 +84,34 @@ app.post('/create-checkout-session', async (req, res) => {
   })
 
   const session = await stripe.checkout.sessions.create({
+    shipping_address_collection: {allowed_countries: ['IN']},
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {amount: 0, currency: 'inr'},
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: {unit: 'business_day', value: 5},
+            maximum: {unit: 'business_day', value: 7},
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {amount: 1500, currency: 'inr'},
+          display_name: 'Next day air',
+          delivery_estimate: {
+            minimum: {unit: 'business_day', value: 1},
+            maximum: {unit: 'business_day', value: 1},
+          },
+        },
+      },
+    ],
+    phone_number_collections:{
+      enabled:true
+    },
     line_items,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
