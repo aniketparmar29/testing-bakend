baa4f74c4a983bd0c8f6f086dff0a4b642342087
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-13 10:37:01 +0530

    Aasda

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const Stripe = require('stripe');
 
 
 dotenv.config({path:"./config.env"})
@@ -17,7 +16,6 @@ dotenv.config({path:"./config.env"})
   //   credentials: true,
   // }));
   
-const stripe = Stripe(process.env.STRIPE_KEY)
 
 app.use(cors())
 
@@ -63,52 +61,6 @@ pool.getConnection(function(err, connection) {
 
 //stripe
 
-app.post('/create-checkout-session', async (req, res) => {
-
-  const line_items  = req.body.cartItems.map(item =>{
-    return {
-      price_data: {
-        currency: 'inr',
-        product_data: {
-          name: item.pr_name,
-          images:[item.pr_img],
-          metadata:{
-            id:item.pr_id,
-            weight:item.pr_weight
-          }
-        },
-        unit_amount: item.pr_price*100,
-      },
-      quantity: item.pr_que,
-    }
-  })
-
-  const session = await stripe.checkout.sessions.create({
-    shipping_address_collection: {allowed_countries: ['IN','US']},
-    shipping_options: [
-      {
-        shipping_rate_data: {
-          type: 'fixed_amount',
-          fixed_amount: {amount: 0, currency: 'inr'},
-          display_name: 'Free shipping',
-          delivery_estimate: {
-            minimum: {unit: 'business_day', value: 5},
-            maximum: {unit: 'business_day', value: 7},
-          },
-        },
-      }
-    ],
-    phone_number_collection:{
-      enabled:true
-    },
-    line_items,
-    mode: 'payment',
-    success_url: `${process.env.CLIENT_URL}/checkout-success`,
-    cancel_url: `${process.env.CLIENT_URL}/cart`,
-  });
-
-  res.send({url: session.url}); 
-});
 
 
 
