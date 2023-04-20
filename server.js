cee15f7a13ebef594f9a26865a3d65fc2da6035c
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-20 23:38:24 +0530

    ASD

--- server.js
+++ server.js
@@ -387,10 +387,9 @@ app.post('/register', (req, res) => {
       }
 
       const role = 'user';
-      const address =0;
       // Insert new user into MySQL database
-      const insertUserSql = 'INSERT INTO users (email, password, name, role, address) VALUES (?, ?, ?, ?, ?)';
-      const insertUserValues = [email, hash, name, role,address];
+      const insertUserSql = 'INSERT INTO users (email, password, name, role) VALUES (?, ?, ?, ?)';
+      const insertUserValues = [email, hash, name, role];
 
       pool.query(insertUserSql, insertUserValues, (error, results) => {
         if (error) {
